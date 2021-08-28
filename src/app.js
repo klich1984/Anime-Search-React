bd063d8c3f1fda3af89fe8d67d200b3cf4a62b8f
Carlos Usuga Martinez <klich84@gmail.com>
2021-08-28 16:57:59 -0500

    Update and delete coments

--- src/app.js
+++ src/app.js
@@ -11,18 +11,12 @@ function saveTask(e) {
   e.preventDefault()
   title = $d.getElementById('title').value
   description = $d.getElementById('description').value
-  // console.log(title, description)
+
   const task = {
     title,
     description
   }
-  // console.log(task)
-  // Guardar datos como string en el local storage
-  // localStorage.setItem('tasks', JSON.stringify(task))
-  // Obtener datos como objeto del local storage
-  // listTask = JSON.parse(localStorage.getItem('tasks'))
-  // console.log(listTask)
-  // Si ya hay tareas agrega tareas si no actualiza y guardalas
+  // If there are already tasks add tasks if not update and save them
   if (localStorage.getItem("tasks") === null) {
     let listTask = []
     listTask.push(task)
@@ -32,7 +26,7 @@ function saveTask(e) {
     readTask.push(task)
     localStorage.setItem('tasks', JSON.stringify(readTask))
   }
-  // Borrar Formulario
+  // Delete Form
   $d.getElementById('formTask').reset()
   getTasks()
 }
@@ -45,7 +39,6 @@ function getTasks() {
   tasksView.innerHTML = ''
 
   for (let i = 0; i < tasks.length; i++) {
-    // console.log(tasks[i])
     let title = tasks[i].title
     let description = tasks[i].description
 
@@ -62,7 +55,6 @@ function getTasks() {
 
 // Delete tTasks
 function deleteTask(title) {
-  // console.log(title)
   let tasks = JSON.parse(localStorage.getItem('tasks'))
 
   for (let i = 0; i < tasks.length; i++) {
